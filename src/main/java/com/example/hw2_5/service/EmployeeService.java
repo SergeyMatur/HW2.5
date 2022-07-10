0a1eb5424499d7df16fa832b0d526dbb16cf81dc
Sergey_Stepanov <cmi52@mail.ru>
2022-07-10 23:25:55 +0300

    HW2.5

--- src/main/java/com/example/hw2_5/service/EmployeeService.java
+++ src/main/java/com/example/hw2_5/service/EmployeeService.java
@@ -7,52 +7,47 @@ import com.example.hw2_5.model.Employee;
 import org.springframework.stereotype.Service;
 
 import javax.swing.plaf.SeparatorUI;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Service
 public class EmployeeService {
 
-    private final Employee[] employees = new Employee[10];
+    private static int LIMIT = 10;
+    private static final List<Employee> employees = new ArrayList<>();
+
 
     public Employee addEmployee(String name, String surname) {
         Employee employee = new Employee(name, surname);
-        int index = -1;
-        for (int i = 0; i < employees.length; i++) {
-            if (Objects.equals(employees[i], employee)) {
-                throw new EmployeeAlreadyAddedException();
-            }
-            if (Objects.isNull(employees[i])) {
-                index = i;
-                break;
-            }
+        if (employees.contains(employee)) {
+            throw new EmployeeAlreadyAddedException();
         }
-        if (index != -1) {
-            employees[index] = employee;
+        if (employees.size() < LIMIT) {
+            employees.add(employee);
             return employee;
-        } throw new EmployeeStorageIsFullException();
+        }
+        throw new EmployeeStorageIsFullException();
     }
 
     public Employee findEmployee(String name, String surname) {
         Employee employee = new Employee(name, surname);
-        for (Employee emp : employees) {
-            if (Objects.equals(emp, employee)) {
-                return employee;
-            }
+        if (!employees.contains(employee)) {
+            throw new EmployeeNotFoundException();
         }
-        throw new EmployeeNotFoundException();
+        return employee;
     }
 
     public Employee removeEmployee(String name, String surname) {
         Employee employee = new Employee(name, surname);
-        for (int i = 0; i < employees.length; i++) {
-            if (Objects.equals(employees[i], employee)) {
-                employees[i] = null;
-                return employee;
-            }
+        if (!employees.contains(employee)) {
+            throw new EmployeeNotFoundException();
         }
-        throw new EmployeeNotFoundException();
-
+        employees.remove(employee);
+        return employee;
     }
 
-
+    public static List<Employee> getAll() {
+        return new ArrayList<>(employees);
+    }
 }
