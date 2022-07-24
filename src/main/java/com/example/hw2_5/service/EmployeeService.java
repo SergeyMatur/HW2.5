cfbf6f7012d1dc871ff7f1991b0d0310ca8edb24
Sergey_Stepanov <cmi52@mail.ru>
2022-07-24 17:28:19 +0300

    HW2.7

--- src/main/java/com/example/hw2_5/service/EmployeeService.java
+++ src/main/java/com/example/hw2_5/service/EmployeeService.java
@@ -7,47 +7,50 @@ import com.example.hw2_5.model.Employee;
 import org.springframework.stereotype.Service;
 
 import javax.swing.plaf.SeparatorUI;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 @Service
 public class EmployeeService {
 
     private static int LIMIT = 10;
-    private static final List<Employee> employees = new ArrayList<>();
+    private static final Map<String, Employee> employees = new HashMap<>();
 
 
     public Employee addEmployee(String name, String surname) {
         Employee employee = new Employee(name, surname);
-        if (employees.contains(employee)) {
+        String key = getKey(name, surname);
+        if (employees.containsKey(key)) {
             throw new EmployeeAlreadyAddedException();
         }
         if (employees.size() < LIMIT) {
-            employees.add(employee);
+            employees.put(key,employee);
             return employee;
         }
         throw new EmployeeStorageIsFullException();
     }
 
     public Employee findEmployee(String name, String surname) {
-        Employee employee = new Employee(name, surname);
-        if (!employees.contains(employee)) {
+        String key = getKey(name, surname);
+        if (employees.containsKey(key)) {
             throw new EmployeeNotFoundException();
         }
-        return employee;
+        return employees.get(key);
     }
 
     public Employee removeEmployee(String name, String surname) {
-        Employee employee = new Employee(name, surname);
-        if (!employees.contains(employee)) {
+        String key = getKey(name, surname);
+        if (employees.containsKey(key)) {
             throw new EmployeeNotFoundException();
         }
-        employees.remove(employee);
-        return employee;
+        return employees.remove(key);
     }
 
     public static List<Employee> getAll() {
-        return new ArrayList<>(employees);
+        return new ArrayList<>(employees.values());
+    }
+
+    private String getKey(String name,
+                          String surname) {
+        return name + "|" + surname;
     }
 }
